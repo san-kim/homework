9d206a7ab85d987553983efb747304eb2401dd51
sankimchi <sankim17@mittymonarch.com>
2016-06-27 18:23:21 -0400

    added assignment 1

---
+++ Assignment1/Homework_1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Homework_1
+//
+//  Created by Kiwook Kim on 6/23/16.
+//  Copyright (c) 2016 sankim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Assignment1/Homework_1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Homework_1
+//
+//  Created by Kiwook Kim on 6/23/16.
+//  Copyright (c) 2016 sankim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
